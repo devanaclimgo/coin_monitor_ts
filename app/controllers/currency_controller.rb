095254ad778d654bde6b926b30446ad6511b4ee2
devanaclimgo <anaclimgo@gmail.com>
2025-07-04 16:49:20 -0300

    feat: added currencies and chart data to currency controller

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -1,2 +1,25 @@
 class CurrencyController < ApplicationController
+  def index
+    currencies = [
+      { code: "USD-BRL" },
+      { code: "EUR-BRL" },
+      { code: "BTC-BRL" }
+    ]
+
+    @chart_data = currencies.map do |currency|
+      url = URI("https://economia.awesomeapi.com.br/json/daily/#{currency[:code]}/15")
+      response = Net::HTTP.get(url)
+      data = JSON.parse(response)
+
+      {
+        name: currency[:code],
+        data: data.each_with_object({}) do |entry, hash|
+          date = Time.at(entry["timestamp"].to_i).strftime("%d/%m/%y")
+          hash[date] = entry["high"].to_f
+        end
+      }
+    end
+
+    render json: @chart_data
+  end
 end
