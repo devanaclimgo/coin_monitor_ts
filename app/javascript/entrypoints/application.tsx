6b06fe87152e9e96162c575cec53a596c3a70252
devanaclimgo <anaclimgo@gmail.com>
2025-07-04 12:00:22 -0300

    feat: added DOM root to application entrypoint

--- app/javascript/entrypoints/application.tsx
+++ app/javascript/entrypoints/application.tsx
@@ -2,5 +2,22 @@ import "@hotwired/turbo-rails"
 import "../controllers"
 import "chartkick"
 import "chart.js/auto"
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-console.log("React is working!");
+const App = () => {
+  return (
+    <div>
+      <h1>React is working!</h1>
+      <div id="chart-container"></div>
+    </div>
+  )
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootElement = document.getElementById('root')
+  if (rootElement) {
+    const root = createRoot(rootElement)
+    root.render(<App />)
+  }
+})
