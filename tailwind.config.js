60ed0571756f7072b661587f6fad4b79ddedd5fa
devanaclimgo <anaclimgo@gmail.com>
2025-07-05 20:03:05 -0300

    build: shadcn installed and tailwind, postcss and tsconfig files created

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/views/**/*.html.erb",
+    "./app/javascript/**/*.{js,ts,jsx,tsx}",
+    "./app/helpers/**/*.rb"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
